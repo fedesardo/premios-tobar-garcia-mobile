6711323ed61cdeca72bd3c2f18cd6df3ac6f30b1
Fede Sardo <fedesardo@gmail.com>
2017-07-23 21:50:20 -0300

    actualizando para salir con el boton back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,113 +11,110 @@ import { ReconocimientoPage } from "../pages/reconocimiento/reconocimiento";
 import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+    rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any, icon: string}>;
+    pages: Array<{ title: string, component: any, icon: string }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public push: Push,
-  public alertCtrl: AlertController,  public toastCtrl:ToastController) {
+    backButtonPressedOnceToExit: boolean;
 
-    this.initializeApp();
+    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public push: Push,
+        public alertCtrl: AlertController, public toastCtrl: ToastController) {
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'El Premio', component: HomePage, icon: 'home' },
-      { title: 'Reconocimientos', component: ReconocimientoPage, icon: 'medal' },
-      { title: 'Rubros', component: RubroPage, icon: 'trophy' },
-      { title: 'Jurado', component: JuradoPage, icon: 'person' },
-      { title: 'Reglamento', component: ReglamentoPage, icon: 'book' },
-      { title: 'Contacto', component: ContactoPage, icon:'information-circle' }
-    ];
+        this.initializeApp();
 
-  }
+        // used for an example of ngFor and navigation
+        this.pages = [
+            { title: 'El Premio', component: HomePage, icon: 'home' },
+            { title: 'Reconocimientos', component: ReconocimientoPage, icon: 'medal' },
+            { title: 'Rubros', component: RubroPage, icon: 'trophy' },
+            { title: 'Jurado', component: JuradoPage, icon: 'person' },
+            { title: 'Reglamento', component: ReglamentoPage, icon: 'book' },
+            { title: 'Contacto', component: ContactoPage, icon: 'information-circle' }
+        ];
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-      this.pushSetup();
+    }
 
-
-      var lastTimeBackPress = 0;
-var timePeriodToExit  = 2000;
-
-        this.platform.registerBackButtonAction(() => {
-            // get current active page
-            let view = this.nav.getActive();
-            if (view.component.name == "HomePage") {
-                //Double check to exit app
-                if (new Date().getTime() - lastTimeBackPress < timePeriodToExit) {
-                    this.platform.exitApp(); //Exit from app
-                } else {
-                    let toast = this.toastCtrl.create({
-                        message:  'Presiona nuevamente para salir de Premios Tobar Garcia',
-                        duration: 3000,
-                        position: 'center'
-                    });
-                    toast.present();
-                    lastTimeBackPress = new Date().getTime();
-                }
-            } else {
-                // go to previous page
-                if (view.component.name == 'JuradoDetailPage'){
-                            this.nav.pop();
-                }else {
-                            this.nav.setRoot(HomePage);
-                }
-                
-            }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            this.statusBar.styleDefault();
+            this.platform.registerBackButtonAction(() => this.myHandlerFunction());
+            this.splashScreen.hide();
+            this.pushSetup();
         });
+    }
 
+    pushSetup() {
 
-
-
-
-
-    });
-  }
-
-  pushSetup(){
-
-      const options: PushOptions = {
-          android: {
-              senderID: '832555175894',
-              sound: true,
-              iconColor: '#804000'
-          } 
+        const options: PushOptions = {
+            android: {
+                senderID: '832555175894',
+                sound: true,
+                iconColor: '#804000'
+            }
         };
 
-      const pushObject: PushObject = this.push.init(options);
+        const pushObject: PushObject = this.push.init(options);
 
-      pushObject.on('notification').subscribe((notification: any) => {
-       // if(notification.additionalData.foregroud){
+        pushObject.on('notification').subscribe((notification: any) => {
+            // if(notification.additionalData.foregroud){
 
-          let pushAlert = this.alertCtrl.create({
-            title: notification.title,
-            message: notification.message,
-            buttons: ['OK']
-          });
-          pushAlert.present();
+            let pushAlert = this.alertCtrl.create({
+                title: notification.title,
+                message: notification.message,
+                buttons: ['OK']
+            });
+            pushAlert.present();
 
 
-      //  }
-      });
+            //  }
+        });
 
-      pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+        pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
 
-      pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
-  }
+        pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+    }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.push(page.component);
+    }
+
+    myHandlerFunction() {
+        console.log("estoy en myfunction");
+        if (this.backButtonPressedOnceToExit) {
+            this.platform.exitApp();
+        } else if (this.nav.canGoBack()) {
+            this.nav.pop({});
+        }
+        else {
+            this.showToast();
+            this.backButtonPressedOnceToExit = true;
+            setTimeout(() => {
+
+                this.backButtonPressedOnceToExit = false;
+            }, 2000)
+        }
+    }
+
+
+    showToast() {
+        let toast = this.toastCtrl.create({
+            message: 'Presione nuevamente para salir',
+            duration: 2000,
+            position: 'bottom'
+        });
+        toast.present();
+    }
+
+
+
+
 }
